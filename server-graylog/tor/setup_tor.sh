f5add202e1538cdbacd2b2c62a762f3db61f783a
katet_3 <colpakov.vadik@yandex.ru>
2024-05-19 18:50:50 +0700

    add tor docker

--- server-graylog/tor/setup_tor.sh
+++ server-graylog/tor/setup_tor.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-
-# Обновление списка пакетов и установка Tor
-sudo apt update
-sudo apt install -y tor
-
-# Проверка и запуск службы Tor
-sudo systemctl start tor
-sudo systemctl enable tor
-
-# Настройка конфигурационного файла Tor
-sudo bash -c 'cat <<EOF >/etc/tor/torrc
-## Tor configuration
-AutomapHostsOnResolve 1
-TransPort 0.0.0.0:9040
-DNSPort 0.0.0.0:53
-SOCKSPort 9050
-EOF'
-
-# Перезапуск службы Tor для применения новых настроек
-sudo systemctl restart tor
-
-# Проверка, что Tor запущен и слушает на нужных портах
-sudo ss -plnt | grep -E '9050|9040|53'
-
 # Создание systemd unit override для Docker
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo bash -c 'cat <<EOF >/etc/systemd/system/docker.service.d/http-proxy.conf
