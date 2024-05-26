12f1a6e51a96b4220778b9d66d7e1b95977bbb9e
katet_3 <colpakov.vadik@yandex.ru>
2024-05-26 16:18:27 +0700

    new docker-compose

--- server-graylog/start.sh
+++ server-graylog/start.sh
@@ -1,7 +1,9 @@
 sudo mkdir /mongo_data
-sudo mkdir /es_data
+sudo mkdir /os_data
+sudo mkdir /os_dashboard_data
 sudo mkdir /graylog_journal
 sudo chmod 777 -R /mongo_data
-sudo chmod 777 -R /es_data
+sudo chmod 777 -R /os_data
+sudo chmod 777 -R /os_dashboard_data
 sudo chmod 777 -R /graylog_journal
 sudo docker-compose up -d
--- server-graylog/tor/up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./tor/cfg ./tor/data
-sudo echo 'SOCKSPort 0.0.0.0:9050'$'\n''DataDirectory /var/lib/tor' > ./tor/cfg/torrc
-sudo chown 100:100 ./tor/data
-sudo docker-compose up -d
